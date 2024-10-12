0a398a9e541840fec6a7314f2fd35815a98be2f1
Armin Aghajani <rminaghajani@gmail.com>
2024-10-12 18:13:28 +0330

    swipe event

--- src/components/tab/TabContainer.jsx
+++ src/components/tab/TabContainer.jsx
@@ -1,4 +1,4 @@
-import { Suspense, useState } from "react";
+import { Suspense, useEffect, useRef, useState } from "react";
 import TabButton from "./TabButton.jsx";
 import { useStyles } from "./TabContainerStyles.js";
 import SkeletonStack from "../skeleton/Skeleton.jsx";
@@ -7,6 +7,48 @@ import { Fragment } from "react";
 export default function TabContainer({ tabs, defaultTab, className }) {
   const [tab, setTab] = useState(defaultTab);
   const styles = useStyles();
+  const tabRef = useRef(null);
+
+  //swipe event
+  useEffect(() => {
+    let element = tabRef.current;
+    let touchstartX = 0;
+    let touchendX = 0;
+
+    function swipe(type) {
+      let indexTab;
+      tabs.find((val, index) => {
+        indexTab = index;
+        return val.label === tab;
+      });
+      if (type === "left" && indexTab !== tabs.length - 1) {
+        setTab(tabs[indexTab + 1].label);
+      } else if (type === "right" && indexTab !== 0) {
+        setTab(tabs[indexTab - 1].label);
+      }
+    }
+
+    function checkDirection() {
+      if (Math.abs(touchendX - touchstartX) >= 80) {
+        if (touchendX < touchstartX) swipe("left");
+        if (touchendX > touchstartX) swipe("right");
+      }
+    }
+    function handleStart(e) {
+      touchstartX = e.changedTouches[0].screenX;
+    }
+    function handleEnd(e) {
+      touchendX = e.changedTouches[0].screenX;
+      checkDirection();
+    }
+
+    element.addEventListener("touchstart", handleStart);
+    element.addEventListener("touchend", handleEnd);
+    return () => {
+      element.removeEventListener("touchstart", handleStart);
+      element.removeEventListener("touchend", handleEnd);
+    };
+  }, [tab, tabs]);
 
   const TabButtons = tabs.map((value, index) => {
     return (
@@ -38,7 +80,7 @@ export default function TabContainer({ tabs, defaultTab, className }) {
 
   return (
     <Suspense fallback={<h1>Loading...</h1>}>
-      <div className={className}>
+      <div className={className} ref={tabRef}>
         <div className={styles.tabContainer}>{TabButtons}</div>
         <>{TabBodies}</>
       </div>
